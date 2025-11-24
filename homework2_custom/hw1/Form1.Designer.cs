678a5759db088ca02497aecb2e22b8782d5e9236
burumbuzer <aarbuzak@gmail.com>
2025-11-25 03:09:40 +0700

    Вторая домашка со звездой

---
+++ homework2_custom/hw1/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace hw1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblMean = new System.Windows.Forms.Label();
+            this.lblMediana = new System.Windows.Forms.Label();
+            this.lblDisp = new System.Windows.Forms.Label();
+            this.lblMode = new System.Windows.Forms.Label();
+            this.tbMu = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbSigma = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(3, 3);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Тесты в интервале";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(577, 443);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(586, 376);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(583, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Количество повторений";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(586, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "10000";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(586, 124);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "50";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(583, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Количество столбцов";
+            // 
+            // lblMean
+            // 
+            this.lblMean.AutoSize = true;
+            this.lblMean.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblMean.Location = new System.Drawing.Point(586, 269);
+            this.lblMean.Name = "lblMean";
+            this.lblMean.Size = new System.Drawing.Size(87, 23);
+            this.lblMean.TabIndex = 6;
+            this.lblMean.Text = "Среднее: ";
+            // 
+            // lblMediana
+            // 
+            this.lblMediana.AutoSize = true;
+            this.lblMediana.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblMediana.Location = new System.Drawing.Point(586, 292);
+            this.lblMediana.Name = "lblMediana";
+            this.lblMediana.Size = new System.Drawing.Size(93, 23);
+            this.lblMediana.TabIndex = 7;
+            this.lblMediana.Text = "Медиана: ";
+            // 
+            // lblDisp
+            // 
+            this.lblDisp.AutoSize = true;
+            this.lblDisp.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblDisp.Location = new System.Drawing.Point(586, 338);
+            this.lblDisp.Name = "lblDisp";
+            this.lblDisp.Size = new System.Drawing.Size(147, 23);
+            this.lblDisp.TabIndex = 8;
+            this.lblDisp.Text = "Выб. Дисперсия: ";
+            // 
+            // lblMode
+            // 
+            this.lblMode.AutoSize = true;
+            this.lblMode.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblMode.Location = new System.Drawing.Point(586, 315);
+            this.lblMode.Name = "lblMode";
+            this.lblMode.Size = new System.Drawing.Size(64, 23);
+            this.lblMode.TabIndex = 9;
+            this.lblMode.Text = "Мода: ";
+            // 
+            // tbMu
+            // 
+            this.tbMu.Location = new System.Drawing.Point(586, 188);
+            this.tbMu.Name = "tbMu";
+            this.tbMu.Size = new System.Drawing.Size(100, 20);
+            this.tbMu.TabIndex = 11;
+            this.tbMu.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(583, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 23);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "μ";
+            // 
+            // tbSigma
+            // 
+            this.tbSigma.Location = new System.Drawing.Point(586, 246);
+            this.tbSigma.Name = "tbSigma";
+            this.tbSigma.Size = new System.Drawing.Size(100, 20);
+            this.tbSigma.TabIndex = 13;
+            this.tbSigma.Text = "1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(583, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 23);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "σ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 450);
+            this.Controls.Add(this.tbSigma);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbMu);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblMode);
+            this.Controls.Add(this.lblDisp);
+            this.Controls.Add(this.lblMediana);
+            this.Controls.Add(this.lblMean);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Разделение на интервалы";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblMean;
+        private System.Windows.Forms.Label lblMediana;
+        private System.Windows.Forms.Label lblDisp;
+        private System.Windows.Forms.Label lblMode;
+        private System.Windows.Forms.TextBox tbMu;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbSigma;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ homework2_custom/hw1/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace hw1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear(); // Очистка диаграммы
+            chart1.ChartAreas[0].AxisX.Minimum = -10; // Зададим границы, чтобы увидеть разницу при изменении Сигмы
+            chart1.ChartAreas[0].AxisX.Maximum = 10;
+
+            Random rnd = new Random();
+
+            double userMu = Convert.ToDouble(tbMu.Text);
+            double userSigma = Convert.ToDouble(tbSigma.Text);
+
+            // Заполняем список "тестов" случайными значениями, записываем максимальное и минимальное значения
+            int arr_size = Convert.ToInt32(textBox1.Text);
+            double[] tests = new double[arr_size];
+
+            // Нормальное распределение с параметрами μ, σ;
+            // Z - СТАНДАРТНАЯ (центр 0, дисп. 1) нормальная случайная величина
+            // (https://w.wiki/GGiD) - преобразование Бокса-Мюллера, при помощи которого получаем Z
+            for (int i = 0; i < arr_size; i++)
+            {
+                double r = 1.0 - rnd.NextDouble(); //NextDouble возвр. значения из [0.0, 1.0)
+                double phi = 1.0 - rnd.NextDouble(); // Отнимем случайное число от 1, чтобы получить интервал (0.0, 1.0]
+                double z = Math.Cos(2.0 * Math.PI * phi) * Math.Sqrt(-2.0 * Math.Log(r)); // Получим стандартную норм. случ. вел.
+                //                  /\
+                //                Первый вариант Метода Бокса - Мюллера, формула Z0
+                //                Переход к общ. нормальному распределению
+                //                  \/
+                tests[i] = userMu + userSigma * z;
+            }
+
+            Array.Sort(tests);
+
+            // Для разделения тестов на интервалы найдём левую и правую границу для них – минимальное и максимальное значение соотв.
+            int columns = Convert.ToInt32(textBox2.Text); // Узнаем кол-во колонок
+            double left_bound = tests.Min();
+            double right_bound = tests.Max();
+            double step = (right_bound - left_bound) / columns;
+
+            for (int b = 0; b < columns; b++)
+            {
+                double left = left_bound + b * step;
+                double right = left + step;
+
+                int countInColumn = 0;
+                for (int i = 0; i < arr_size; i++)
+                {
+                    if (tests[i] >= left && tests[i] < right)
+                        countInColumn++;
+                }
+                Debug.WriteLine($"Точка {left + step / 2:F2}");
+                chart1.Series[0].Points.AddXY(Math.Round(left+step/2), countInColumn);
+            }
+
+
+            double mean = tests.Average(); // Среднее
+
+            double median; // Медиана
+            if (tests.Length % 2 == 0) median = (tests[tests.Length / 2] + tests[tests.Length / 2 + 1]) / 2.0;
+            else median = tests[tests.Length / 2];
+
+            // Поиск моды, cmax – кол-во встреч текущего элемента, rmax - наибольшее значение встреч
+            // Работает, как можно легко понять, ОЧЕНЬ медлнно
+            double mode = tests[0]; 
+            double cmax = 0, rmax = 0;
+            for (int i = 0; i < arr_size; i++)
+            {
+                if (cmax > rmax)
+                {
+                    rmax = cmax;
+                    mode = tests[i - 1];
+                }
+                cmax = 0;
+                for (int j = i; j < arr_size; j++)
+                {
+                    if (Math.Round(tests[j], 3) == Math.Round(tests[i], 3)) cmax++;
+                }
+            }
+
+            double disp = 0; // Поиск дисперсии
+            for (int i = 0; i < arr_size; i++)
+            {
+                disp += (tests[i] - mean) * (tests[i] - mean);
+            }
+            disp = disp / (Convert.ToDouble(arr_size));
+            disp = disp * (Convert.ToDouble(arr_size) / Convert.ToDouble(arr_size - 1));
+
+
+            lblMean.Text = $"Среднее: {mean:F5}";
+            lblMediana.Text = $"Медиана: {median:F5}";
+            lblMode.Text = $"Мода: {mode:F5}";
+            lblDisp.Text = $"Дисперсия: {disp:F5}";
+        }
+    }
+}
